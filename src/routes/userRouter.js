3311d459d0a045b3085d3c25c48acd52b8ff0127
jlucero805 <71980378+jlucero805@users.noreply.github.com>
2021-05-07 21:44:36 -0700

    added jwt processing

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -14,7 +14,7 @@ const authToken = (req, res, next) => {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (token == null) return res.sendStatus(401)
-    jwt.verify(token, proces.env.ACCESS_TOKEN_SECRET, (err, username) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, username) => {
         if (err) return res.sendStatus(403)
         req.username = username 
         next()
