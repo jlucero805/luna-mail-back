e6e7e79a39bb163d85438d1a8d9d41eab60c3d76
jlucero805 <71980378+jlucero805@users.noreply.github.com>
2021-05-08 00:03:15 -0700

    sending mail check if recipient exists

--- src/routes/mailRouter.js
+++ src/routes/mailRouter.js
@@ -39,7 +39,7 @@ mailRouter.post('/', authToken, async (req, res) => {
         const body = req.body
         const checkUser = User.findOne({username: body.to})
         if (!checkUser) {
-            return res.status(400)
+            return res.status(400).json({err: "none"})
         }
         const newMail = new Mail({
             from: req.username.name,
