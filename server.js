4d7320318048063445b00bd41d6b9327cb5273e4
jlucero805 <71980378+jlucero805@users.noreply.github.com>
2021-05-08 02:15:03 -0700

    added git ignore

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const userRouter = require('./src/routes/userRouter')
 const mailRouter = require('./src/routes/mailRouter')
 
 
-const url = 'mongodb://localhost:27017/luna-mail'
-const onlineUrl = 'mongodb+srv://jlucero:<>@cluster0.v3aqi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// const url = 'mongodb://localhost:27017/luna-mail'
+// const onlineUrl = 'mongodb+srv://jlucero:<>@cluster0.v3aqi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose.connect(process.env.DJANGO_PASS, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true})
     .then(() => {console.log('connected to mongo!')})
     .catch((err) => { console.log('mongo connection failed!') })
@@ -18,15 +18,14 @@ app.use((req, res, next) => { console.log(req.method, req.originalUrl); next(); 
 
 app.use(cors())
 app.use(express.json())
+
 //routes
 app.use('/api/users', userRouter)
 app.use('/api/mail', mailRouter)
 app.use('/api/auth', authRouter)
 
-
 const PORT = process.env.PORT || 8080
 // const PORT = 8080
 app.listen(PORT, () => { console.log(`running on port ${PORT}`) })
-
 
 module.exports = app
