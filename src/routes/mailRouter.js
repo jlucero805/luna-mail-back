690f0a02e0b4e03c1154c152f70bfca7e76a0422
jlucero805 <71980378+jlucero805@users.noreply.github.com>
2021-05-07 21:51:31 -0700

    get request using jwt token

--- src/routes/mailRouter.js
+++ src/routes/mailRouter.js
@@ -3,54 +3,25 @@ const jwt = require('jsonwebtoken')
 //Mongo
 const Mail = require('../models/mail')
 
-mailRouter.get('/', async (req, res) => {
-    const mail = await Mail.find({})
-    res.status(200).json(mail)
-})
 
-mailRouter.get('/:username', async (req, res) => {
-    const token = authenticateToken(req)
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-    if (!token || !decoded.name || req.params.username !== decoded.name) {
-        return response.status(401).json({error: 'missing token'})
-    }
-    const user = await Mail.find({to: decoded.name})
-    res.status(200).json(user)
-})
-
-mailRouter.get('/test', async (req, res) => {
-    res.status(200).json({test: "works fine!"})
-})
-
-mailRouter.post('/', async (req, res) => {
-    const body = req.body
-    const newMail = new Mail({
-        from: body.from,
-        to: body.to,
-        title: body.title,
-        content: body.content,
-        hashKey: body.hashKey,
-        dateSent: new Date()
+const authToken = (req, res, next) => {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, username) => {
+        if (err) return res.sendStatus(403)
+        req.username = username
+        next()
     })
-    await newMail.save()
-    res.status(201).json({mail: "sent"})
-})
-
-mailRouter.delete('/', async (req, res) => {
-    await Mail.deleteMany({})
-    res.status(204).json({deleted: "all"})
-})
-
-mailRouter.delete('/:id', async (req, res) => {
-    await Mail.deleteOne({_id: req.params.id})
-    res.status(204).json({deleted: "mail"})
-})
-
-const authenticateToken = (req) => {
-    const authHeader = request.get('authorization')
-    if (!authHeader) return null
-    const token = authHeader.split(' ')[1]
-    return token
 }
+
+mailRouter.get('/', authToken, async (req, res) => {
+    try {
+        const allMail = await Mail.find({ username: req.username })
+        res.status(200).json(allMail)
+    } catch (e) {
+        res.status(400)
+    }
+})
 
 module.exports = mailRouter
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -5,11 +5,6 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 const Mail = require('../models/mail')
 
-userRouter.get('/', async (req, res) => {
-    const all = await User.find({})
-    res.status(200).json(all)
-})
-
 const authToken = (req, res, next) => {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
@@ -20,87 +15,5 @@ const authToken = (req, res, next) => {
         next()
     })
 }
-
-userRouter.get('/test', authToken, async (req, res) => {
-    res.json({works: req.username})
-})
-
-userRouter.get('/:username', async (req, res) => {
-    try {
-        const one = await User.findOne({ username: req.params.username })
-        res.status(200).json(one)
-    } catch (e) {
-        res.status(500).end()
-    }
-})
-
-userRouter.get('/allmail/:username', async (req, res) => {
-    const allUserMail = await Mail.find({ to: req.params.username })
-    return res.status(200).json(allUserMail.reverse())
-})
-
-
-userRouter.get('/allsent/:username', async (req, res) => {
-    const allUserMail = await Mail.find({ from: req.params.username })
-    return res.status(200).json(allUserMail.reverse())
-})
-
-userRouter.post('/', async (req, res) => {
-    const body = req.body
-    try {
-        const hashedPassword = await bcrypt.hash(body.passHash, 10)
-        const newUser = new User({
-            username: body.username,
-            passHash: hashedPassword,
-            contacts: [],
-            dateCreated: new Date()
-        })
-        await newUser.save()
-        const firstMail = new Mail({
-            from: "lunamail",
-            to: body.username,
-            title: "Welcome!",
-            content: `Hey ${body.username}! Welcome to Luna Mail! Try sending a message!`,
-            dateSent: new Date()
-        })
-        await firstMail.save()
-        res.status(201).json({ success: "created" })
-    } catch (err) {
-        res.status(500)
-    }
-})
-
-userRouter.post('/login', async (req, res) => {
-    const body = req.body
-    const user = await User.findOne({ username: body.username })
-    if (user === null) {
-        return res.json({ error: "error" })
-    }
-    try {
-        if (await bcrypt.compare(body.passHash, user.passHash)) {
-            const allUserMail = await Mail.find({ to: body.username })
-            return res.status(200).json(allUserMail.reverse())
-        } else {
-            return res.json({ error: "error" })
-        }
-    } catch {
-        return res.json({ error: "error" })
-    }
-})
-
-userRouter.put('/:username', async (req, res) => {
-    const body = req.body
-    const newUser = {
-        contacts: body.contacts
-    }
-    await User.updateOne({ username: req.params.username }, newUser)
-    res.status(201).json({ user: "updated" })
-})
-
-//delete all, Dont use!
-userRouter.delete('/', async (req, res) => {
-    await User.deleteMany({})
-    res.status(204).json({ all: "deleted" })
-})
 
 module.exports = userRouter
