6e1113825f5ea473511a2d4a6ffea67251e8046f
jason <jluce@DESKTOP-LOTUM80.localdomain>
2021-05-14 23:28:14 -0700

    test cases for mail router

--- tests/mail.test.js
+++ tests/mail.test.js
@@ -4,25 +4,23 @@ const app = require('../server')
 
 const api = supertest(app)
 
-test('token is sent back', async () => {
-    await api
-        .post('/api/auth/login')
-        .send({
-            username: "jason",
-            passHash: "jason"
-        })
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-})
-
 test('GET /api/users', async () => {
     const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiamFzb24iLCJpYXQiOjE2MjA0NDk5NjV9.HyFlJpiTrmMKOey3T-g2v75BYDoCKXfjhtzuqCJNpSQ"
     await api
         .get('/api/users')
         .set("Authorization", "bearer " + token)
-        .expect(200, {name: { name: "jason", iat: 1620449965}})
+        .expect(200)
         .expect('Content-Type', /application\/json/)
 })
+
+// test('token is sent back', async () => {
+//     const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiamFzb24iLCJpYXQiOjE2MjA0NDk5NjV9.HyFlJpiTrmMKOey3T-g2v75BYDoCKXfjhtzuqCJNpSQ"
+//     await api
+//         .get('/api/users/sent')
+//         .set("Authorization", "bearer " + token)
+//         .expect(200)
+//         .expect('Content-Type', /application\/json/)
+// })
 
 afterAll(() => {
     mongoose.connection.close()
