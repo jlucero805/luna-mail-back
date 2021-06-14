315be761d32bd84f99ac40a2e4ea38d297dd82cb
jlucero805 <71980378+jlucero805@users.noreply.github.com>
2021-06-13 17:06:28 -0700

    added GET /contacts

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -71,4 +71,14 @@ userRouter.get('/contacts', authToken, async (req, res) => {
     }
 })
 
+userRouter.put('/contacts', authToken, async (req, res) => {
+    const body = req.body;
+    try {
+        await User.updateOne({username: req.username.name}, {contacts: body.contacts});
+        res.status(200);
+    }  catch (e) {
+        res.status(400);
+    }
+})
+
 module.exports = userRouter
